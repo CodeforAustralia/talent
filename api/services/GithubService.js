6cb8ca468ae84c9dcb7fa8c7b19b45a2ec1194ac
Ezekiel Kigbo <theatlasroom@gmail.com>
2016-03-24 19:16:27 +1100

    basic fetch and update user activty working

--- api/services/GithubService.js
+++ api/services/GithubService.js
@@ -1,3 +1,5 @@
+var request = require('request');
+var cheerio = require('cheerio');
 var unirest = require('unirest'),
 		GithubAPI = 'https://api.github.com',
 		OAUTH_TOKEN = 'be6f7b4f9d19f5748fff452e31faf3655e183a6d'
@@ -48,36 +50,75 @@ module.exports = {
 					 .header('User-Agent', 'dgroch')
 					 .header('Authorization', `Token ${OAUTH_TOKEN}`)
 					 .end(function(results) {
-						 	var newDeveloper = {
-						 		username: 					results.body.login || '',
-						 	 	github_id: 					parseInt(results.body.id) || '',
-						 	 	name: 							results.body.name || '',
-						 	 	avatar_url: 				results.body.avatar_url || '',
-						 	 	company: 						results.body.company || '',
-						 	 	blog_url: 					results.body.blog || '',
-						 	 	location: 					results.body.location || '',
-						 	 	email: 							results.body.email || '',
-						 	 	url: 								results.body.url || '',
-						 	 	html_url: 					results.body.html_url || '',
-						 	 	public_repos: 			parseInt(results.body.public_repos) || 0,
-						 	 	public_gists: 			parseInt(results.body.public_gists) || 0,
-						 	 	followers: 					parseInt(results.body.followers) || 0,
-						 	 	following: 					parseInt(results.body.following) || 0,
-						 	 	followers_url: 			results.body.followers_url || '',
-						 	 	following_url: 			results.body.following_url || '',
-						 	 	gists_url: 					results.body.gists_url || '',
-						 	 	subscriptions_url: 	results.body.subscriptions_url || '',
-						 	 	organisations_url: 	results.body.organisations_url || '',
-						 	 	repos_url: 					results.body.repos_url || ''
-						 	}
+						 	// check if the dev exists and skip if they do
+							Developer.findOne({'username': results.body.login}).exec(function(_err, _res){
+								if (_res){
+									console.log(results.body.login + " already exists");
+									return;
+								}
 
-					 	 Developer.create(newDeveloper).exec(function(err, model) {
+							 	var newDeveloper = {
+							 		username: 					results.body.login || '',
+							 	 	github_id: 					parseInt(results.body.id) || '',
+							 	 	name: 							results.body.name || '',
+							 	 	avatar_url: 				results.body.avatar_url || '',
+							 	 	company: 						results.body.company || '',
+							 	 	blog_url: 					results.body.blog || '',
+							 	 	location: 					results.body.location || '',
+							 	 	email: 							results.body.email || '',
+							 	 	url: 								results.body.url || '',
+							 	 	html_url: 					results.body.html_url || '',
+							 	 	public_repos: 			parseInt(results.body.public_repos) || 0,
+							 	 	public_gists: 			parseInt(results.body.public_gists) || 0,
+							 	 	followers: 					parseInt(results.body.followers) || 0,
+							 	 	following: 					parseInt(results.body.following) || 0,
+							 	 	followers_url: 			results.body.followers_url || '',
+							 	 	following_url: 			results.body.following_url || '',
+							 	 	gists_url: 					results.body.gists_url || '',
+							 	 	subscriptions_url: 	results.body.subscriptions_url || '',
+							 	 	organisations_url: 	results.body.organisations_url || '',
+							 	 	repos_url: 					results.body.repos_url || '',
+									activity: 0
+							 };
+
+							// store the user
+							Developer.create(newDeveloper).exec(function(err, model) {
 						 		if (err) {
-									console.log(err)
+									console.log(err);
 								} else {
-									console.log(`added ${model.username}`)
+									console.log(`added ${model.username}`);
 								}
 							});
+						});
 					 });
+	},
+
+	updateActivity: function(id){
+		// returns a users github activity
+		// get the users activity
+		var profile = `https://github.com/${id}`;
+		request.get(profile, function(error, response, body){
+
+			if (error) console.error(error);
+
+			//console.log(body);
+			$ = cheerio.load(body);
+			var sel = "#contributions-calendar g rect";
+			// get the days that are not empty
+			var act = $(sel).not("[data-count='0']");
+			var activity = act.length;
+			// update their activity
+			if (activity > 0){
+				Developer
+					.update({'username': id}, {'activity': activity})
+					.exec(function(err, res){
+						if (err){
+							console.log(err);
+						} else {
+							console.log(`Updated ${id} activity ${activity}`);
+						}
+					});
+			}
+		});
 	}
 }
